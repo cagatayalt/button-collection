571f4428f106d3e35f593f213e0e289c57b7f2af
cagatayalt <caltuntas@itu.edu.tr>
2022-07-31 12:03:19 +0300

    Partial Gradient btn

--- app/src/main/java/com/example/buttoncollection/MainActivity.kt
+++ app/src/main/java/com/example/buttoncollection/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.buttoncollection
 
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Shader
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +13,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+
+
+        val tv2 = findViewById<TextView>(R.id.feedBtnTv2)
+        val paint = tv2.paint
+
+        val width = paint.measureText(tv2.text.toString())
+        val textShader: Shader = LinearGradient(
+            0f, 0f, width, tv2.textSize, intArrayOf(
+                Color.parseColor("#641AF5"),
+                Color.parseColor("#296EFF"),
+            ), null, Shader.TileMode.REPEAT
+        )
+
+        tv2.paint.shader = textShader
 
 
 
